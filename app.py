c77e2fdc19da4ae76691f6cdeac715723942342f
Walter <wjesus88@gmail.com>
2024-08-21 17:42:19 -0500

    upload files

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World! This is a Flask app deployed with GitHub Actions on EC2."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
