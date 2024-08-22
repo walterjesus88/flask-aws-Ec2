eceea9f3615398ac00267e84c2fd6aa2b7d2531a
Walter <wjesus88@gmail.com>
2024-08-22 18:19:37 -0500

    dd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a Flask app deployed with GitHub Actions on EC2."
+    return "Hello, Worded! This is a Flask app deployed with GitHub Actions on EC2."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
