695cdd41cff6fdcd8580197cedacadd77a241ace
Walter <wjesus88@gmail.com>
2024-08-26 16:04:25 -0500

    x_x

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, tions on EC2."
+    return "Hello, estamos en GitHub Actions on EC2."
 
 if __name__ == '__main__':
     app.run()
