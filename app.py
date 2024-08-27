a8c540085118e93ef682b527188d5cb39ac2f1d6
Walter <wjesus88@gmail.com>
2024-08-27 14:06:31 -0500

    change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, actions on EC2."
+    return "Hello, soy un developer maste. Deploy on EC2."
 
 if __name__ == '__main__':
     app.run()
