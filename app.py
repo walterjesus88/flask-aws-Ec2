1ca9d4d2f4cbe2afd71b72206f13c0a78b425f77
Walter <wjesus88@gmail.com>
2024-08-27 14:08:12 -0500

    aqui

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, soy un developer maste. Deploy on EC2."
+    return "Carlos es un idiota!!"
 
 if __name__ == '__main__':
     app.run()
